313d4958a7a7f9403b86a6d3bc3037f8b980e7ae
Soyoung Gwak <rhkrthdud7@gmail.com>
2020-02-29 23:50:22 +0900

    init

---
+++ Chatting/RIBs/Root/RootRouter.swift
@@ -0,0 +1,27 @@
+//
+//  RootRouter.swift
+//  Chatting
+//
+//  Created by Soso on 29/02/2020.
+//  Copyright © 2020 Soso. All rights reserved.
+//
+
+import RIBs
+
+protocol RootInteractable: Interactable {
+    var router: RootRouting? { get set }
+    var listener: RootListener? { get set }
+}
+
+protocol RootViewControllable: ViewControllable {
+    // TODO: Declare methods the router invokes to manipulate the view hierarchy.
+}
+
+final class RootRouter: LaunchRouter<RootInteractable, RootViewControllable>, RootRouting {
+
+    // TODO: Constructor inject child builder protocols to allow building children.
+    override init(interactor: RootInteractable, viewController: RootViewControllable) {
+        super.init(interactor: interactor, viewController: viewController)
+        interactor.router = self
+    }
+}
---
+++ Podfile
@@ -0,0 +1,15 @@
+# Uncomment the next line to define a global platform for your project
+platform :ios, '13.0'
+
+target 'Chatting' do
+  # Comment the next line if you're not using Swift and don't want to use dynamic frameworks
+  use_frameworks!
+  inhibit_all_warnings!
+
+  # Pods for Chatting
+  pod 'RIBs', '~> 0.9'
+  pod 'RxSwift'
+  pod 'RxCocoa'
+  pod 'SnapKit'
+
+end
