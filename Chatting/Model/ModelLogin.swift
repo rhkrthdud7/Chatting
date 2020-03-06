ae1fada4d2d1f3a3b036000ef19a40df576c468a
Soyoung Gwak <rhkrthdud7@gmail.com>
2020-03-06 20:07:27 +0900

    added ModelLogin

---
+++ Chatting/Model/ModelLogin.swift
@@ -0,0 +1,13 @@
+//
+//  ModelLogin.swift
+//  Chatting
+//
+//  Created by Soso on 01/03/2020.
+//  Copyright © 2020 Soso. All rights reserved.
+//
+
+import Foundation
+
+struct ModelLogin: Codable {
+    let token: String
+}
