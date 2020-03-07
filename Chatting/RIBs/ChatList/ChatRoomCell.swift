e08c9c308a5d0d069da7331547f8c23282992fd6
Soyoung Gwak <rhkrthdud7@gmail.com>
2020-03-07 18:38:57 +0900

    changed ChatRoomCell profile image masking logic

--- Chatting/RIBs/ChatList/ChatRoomCell.swift
+++ Chatting/RIBs/ChatList/ChatRoomCell.swift
@@ -28,15 +28,14 @@ class ChatRoomCell: UITableViewCell {
     
     func configure(data: ChatRoomViewModel) {
         let imageViews = [imageView1, imageView2, imageView3, imageView4]
-        setImageViewFrames(imageViews: Array(imageViews[0..<data.imageURLs.count]))
+        setFrameAndRadius(imageViews: Array(imageViews[0..<data.imageURLs.count]))
         for (index, _) in data.imageURLs.enumerated() {
             let imageView = imageViews[index]
             imageView.image = #imageLiteral(resourceName: "image_profile_default")
-            imageView.roundedCorners()
         }
     }
     
-    func setImageViewFrames(imageViews: [UIImageView]) {
+    func setFrameAndRadius(imageViews: [UIImageView]) {
         let mid = viewProfile.bounds.midX
         let max = viewProfile.bounds.maxX
         if imageViews.count == 1 {
@@ -44,13 +43,13 @@ class ChatRoomCell: UITableViewCell {
             imageViews[0].frame = CGRect(x: 0, y: 0, width: value, height: value)
         } else if imageViews.count == 2 {
             let value: CGFloat = 35
-            imageViews[0].frame = CGRect(x: 0, y: 0, width: value, height: value)
-            imageViews[1].frame = CGRect(x: max-value, y: max-value, width: value, height: value)
+            imageViews[0].frame = CGRect(x: max-value, y: max-value, width: value, height: value)
+            imageViews[1].frame = CGRect(x: 0, y: 0, width: value, height: value)
         } else if imageViews.count == 3 {
             let value: CGFloat = 30
-            imageViews[0].frame = CGRect(x: 0, y: max-value, width: value, height: value)
+            imageViews[0].frame = CGRect(x: mid-value/2, y: 0, width: value, height: value)
             imageViews[1].frame = CGRect(x: max-value, y: max-value, width: value, height: value)
-            imageViews[2].frame = CGRect(x: mid-value/2, y: 0, width: value, height: value)
+            imageViews[2].frame = CGRect(x: 0, y: max-value, width: value, height: value)
         } else if imageViews.count == 4 {
             let value: CGFloat = 28
             imageViews[0].frame = CGRect(x: 0, y: 0, width: value, height: value)
@@ -58,7 +57,62 @@ class ChatRoomCell: UITableViewCell {
             imageViews[2].frame = CGRect(x: 0, y: max-value, width: value, height: value)
             imageViews[3].frame = CGRect(x: max-value, y: max-value, width: value, height: value)
         }
+        
+        for (current, imageView) in imageViews.enumerated() {
+            let ratio: CGFloat = 0.15
+            let inset: CGFloat = 2
+            
+            let bounds = imageView.bounds
+            let width = bounds.width
+            let height = bounds.height
+            
+            let pathRect = UIBezierPath(rect: bounds)
+            let pathOriginal = UIBezierPath()
+            let pathClip = UIBezierPath()
+            
+            let topCenter = CGPoint(x: bounds.midX, y: 0)
+            let leftCenter = CGPoint(x: 0, y: bounds.midY)
+            let bottomCenter = CGPoint(x: bounds.midX, y: height)
+            let rightCenter = CGPoint(x: width, y: bounds.midY)
+            pathOriginal.move(to: topCenter)
+            pathOriginal.addCurve(to: leftCenter,
+                          controlPoint1: CGPoint(x: width*ratio, y: 0),
+                          controlPoint2: CGPoint(x: 0, y: height*ratio))
+            pathOriginal.addCurve(to: bottomCenter,
+                          controlPoint1: CGPoint(x: 0, y: height*(1-ratio)),
+                          controlPoint2: CGPoint(x: width*ratio, y: height))
+            pathOriginal.addCurve(to: rightCenter,
+                          controlPoint1: CGPoint(x: width*(1-ratio), y: height),
+                          controlPoint2: CGPoint(x: width, y: height*(1-ratio)))
+            pathOriginal.addCurve(to: topCenter,
+                          controlPoint1: CGPoint(x: width, y: height*ratio),
+                          controlPoint2: CGPoint(x: width*(1-ratio), y: 0))
+            
+            let factor = (bounds.width - inset * 2) / bounds.width
+            let transform = CGAffineTransform(scaleX: factor, y: factor).translatedBy(x: inset, y: inset)
+            
+            let pathMask = UIBezierPath(cgPath: pathOriginal.cgPath)
+            pathMask.apply(transform)
+            
+            for index in 0..<current {
+                let path = UIBezierPath(cgPath: pathOriginal.cgPath)
+                let x = imageViews[index].frame.minX - imageView.frame.minX
+                let y = imageViews[index].frame.minY - imageView.frame.minY
+                let translate = CGAffineTransform(translationX: x, y: y)
+                path.apply(translate)
+                pathClip.append(path)
+            }
+
+            pathRect.append(pathClip)
+            let image = UIGraphicsImageRenderer(bounds: bounds).image { (c) in
+                pathRect.addClip()
+                pathMask.fill()
+            }
+            
+            let layer = CALayer()
+            layer.frame = bounds
+            layer.contents = image.cgImage
+            imageView.layer.mask = layer
+        }
     }
 }
-
-// 54 35 30 28
