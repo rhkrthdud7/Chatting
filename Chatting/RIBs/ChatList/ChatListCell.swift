0b4f790440de5a68fb832f6cb9be3229f6153979
Soyoung Gwak <rhkrthdud7@gmail.com>
2020-03-07 22:13:05 +0900

    added comments for ChatListCell profileImageViews setup logic

--- Chatting/RIBs/ChatList/ChatListCell.swift
+++ Chatting/RIBs/ChatList/ChatListCell.swift
@@ -35,9 +35,11 @@ class ChatListCell: UITableViewCell {
         }
     }
     
+    // from one to four profile imageViews
     func setFrameAndRadius(imageViews: [UIImageView]) {
         let mid = viewProfile.bounds.midX
         let max = viewProfile.bounds.maxX
+        // set imageViews's frames based on count
         if imageViews.count == 1 {
             let value: CGFloat = 54
             imageViews[0].frame = CGRect(x: 0, y: 0, width: value, height: value)
@@ -58,6 +60,7 @@ class ChatListCell: UITableViewCell {
             imageViews[3].frame = CGRect(x: max-value, y: max-value, width: value, height: value)
         }
         
+        // set masking layer for each imageView
         for (current, imageView) in imageViews.enumerated() {
             let ratio: CGFloat = 0.15
             let inset: CGFloat = 2
@@ -66,10 +69,11 @@ class ChatListCell: UITableViewCell {
             let width = bounds.width
             let height = bounds.height
             
-            let pathRect = UIBezierPath(rect: bounds)
             let pathOriginal = UIBezierPath()
+            let pathRect = UIBezierPath(rect: bounds)
             let pathClip = UIBezierPath()
             
+            // bezierPath for the main path with custom radius
             let topCenter = CGPoint(x: bounds.midX, y: 0)
             let leftCenter = CGPoint(x: 0, y: bounds.midY)
             let bottomCenter = CGPoint(x: bounds.midX, y: height)
@@ -88,12 +92,13 @@ class ChatListCell: UITableViewCell {
                           controlPoint1: CGPoint(x: width, y: height*ratio),
                           controlPoint2: CGPoint(x: width*(1-ratio), y: 0))
             
+            // add 2pt inset
             let factor = (bounds.width - inset * 2) / bounds.width
             let transform = CGAffineTransform(scaleX: factor, y: factor).translatedBy(x: inset, y: inset)
-            
             let pathMask = UIBezierPath(cgPath: pathOriginal.cgPath)
             pathMask.apply(transform)
             
+            // add clipping for lower imageViews
             for index in 0..<current {
                 let path = UIBezierPath(cgPath: pathOriginal.cgPath)
                 let x = imageViews[index].frame.minX - imageView.frame.minX
@@ -103,12 +108,17 @@ class ChatListCell: UITableViewCell {
                 pathClip.append(path)
             }
 
+            // append clipping area to the bounding rect of the current imageView
             pathRect.append(pathClip)
+            // get new image for masking layer
             let image = UIGraphicsImageRenderer(bounds: bounds).image { (c) in
+                // add clip
                 pathRect.addClip()
+                // fill non-clipped area
                 pathMask.fill()
             }
             
+            // set layer mask
             let layer = CALayer()
             layer.frame = bounds
             layer.contents = image.cgImage
